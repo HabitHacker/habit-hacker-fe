a1337443a201e3d5b488dfe9d986f3c574d63667
JeinJang <jein.jang@poomang.com>
2023-04-14 19:42:01 +0900

    repository created

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import App from 'next/app';
+
+function app({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  );
+}
+   
+app.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  
+  return { ...appProps }
+}
+
+export default app;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Image Upload</title>
+      </Head>
+      <div>
+        <h1>Image Upload</h1>
+      </div>
+    </>
+  );
+};
